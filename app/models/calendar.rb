43f98ca51f8f91b92466e6d9206532b3dab661f2
yunayotuba <73088676+yunayotuba@users.noreply.github.com>
2020-11-13 17:47:20 +0900

    calendarモデルのマイグレート

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,7 +1,7 @@
 class Calendar < ApplicationRecord
-  belongs_to :user
-  has_many :bookmark_calendars
-  has_many :bookmarks, through: :bookmark_calendars
+  #belongs_to :user
+  #has_many :bookmark_calendars
+  #has_many :bookmarks, through: :bookmark_calendars
 
   validates :date, presence: true
 end
--- db/migrate/20201113074339_create_calendars.rb
+++ db/migrate/20201113074339_create_calendars.rb
@@ -2,7 +2,7 @@ class CreateCalendars < ActiveRecord::Migration[6.0]
   def change
     create_table :calendars do |t|
       t.date :date, null: false
-      t.references :user, foreign_key: true
+      #t.references :user, foreign_key: true
       t.timestamps
     end
   end
