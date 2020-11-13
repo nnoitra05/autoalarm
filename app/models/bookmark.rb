89977f3d1898e345beb3ac5578eb2bb04aa0c5e0
yunayotuba <73088676+yunayotuba@users.noreply.github.com>
2020-11-13 18:14:10 +0900

    Bookmarkモデルの実装、マイグレーションファイルの設定

---
+++ app/models/bookmark.rb
@@ -0,0 +1,11 @@
+class Bookmark < ApplicationRecord
+  #belongs_to :user
+  #has_many :bookmark_calendars
+  #has_many :calendars, through: :bookmark_calendars
+
+  validates :date, presence: true
+  validates :departure, presence: true
+  validates :destination, presence: true
+  validates :time, presence: true
+  validates :status_check, presence: true 
+end
