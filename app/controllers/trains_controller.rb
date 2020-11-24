9a93bdf0c7b162574049326e73eff3de230aa838
nnoitra05 <nexcoxin@gmail.com>
2020-11-24 12:56:22 +0900

    経路検索結果の仮置き及びgemのインストール

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -8,5 +8,19 @@ class TrainsController < ApplicationController
   end
 
   def search
+
+    # まだ経路検索実装できてないので、検索するとこういうレスポンスが返ってくるという想定で@responseにハッシュを記述しておきます。
+    # 時刻は全てDateTime.nowで仮置きしてます。
+    # これを加工してtrains/searchビューに反映させるイメージで！
+    @response = {
+      departure: "西国分寺",
+      destination: "渋谷",
+      time: DateTime.now,
+      routes: [
+        {departure: "西国分寺", destination: "新宿", departure_time: DateTime.now, arrival_time: DateTime.now}, 
+        {departure: "新宿", destination: "渋谷", departure_time: DateTime.now, arrival_time: DateTime.now}
+      ]  
+    }
+
   end
 end
