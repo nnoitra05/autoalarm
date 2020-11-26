8d728e60dad43544ab728105e34b21edc8cc3ce9
nnoitra05 <nexcoxin@gmail.com>
2020-11-26 13:22:54 +0900

    運行情報ビュー表示及びアクションの記述修正

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -2,8 +2,21 @@ class TrainsController < ApplicationController
 
   def index
 
-    @train_information = FetchTrainsApisService.fetch(FetchTrainsApisService.apis[:ti])
+    train_information = FetchTrainsApisService.fetch(FetchTrainsApisService.apis[:ti])
     @information_list = []
+
+    train_information.each do |train_info|
+
+      train_status_str = "odpt:trainInformationStatus"
+      train_infomation_text_str = "odpt:trainInformationText"
+
+      if train_info.keys.include?(train_status_str) && !train_info[train_status_str].nil?
+        @information_list << train_info[train_infomation_text_str]["ja"]
+      end
+
+    end
+
+    @information_list = @information_list.uniq
 
   end
 
