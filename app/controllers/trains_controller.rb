a05c74a88e8fa3bdfa18e390eca52ed97ef5456a
jxn1156596 <jxn1156596@hotmail.com>
2020-12-03 09:34:59 +0900

    test

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -19,13 +19,13 @@ class TrainsController < ApplicationController
     departure_flag = params[:departure_flag].to_i
 
     # サービスクラスSearchNavitimeRoutesServiceから条件を満たす最適なルートを取得
-    # @route_result = SearchNavitimeRoutesService.fetch(departure, destination, time, departure_flag)
+    @route_result = SearchNavitimeRoutesService.fetch(departure, destination, time, departure_flag)
 
     # NAVITIME APIのコール回数削減のために経路検索結果をdumpしたものです。特に不要な場合はこちらのメソッドで読込してください。
     # 直通か非直通かでそれぞれjsonファイルが違うので、目的に合わせてコメントアウトを外してください。
     # file_name = Rails.public_path.join("jsons", "response_sample_no_transit.json") # 所沢→渋谷の直通経路のレスポンス
-    file_name = Rails.public_path.join("jsons", "response_sample.json") # 西国分寺→渋谷の乗換有のレスポンス
-    @route_result = SearchNavitimeRoutesService.sample_fetch(file_name)
+    # file_name = Rails.public_path.join("jsons", "response_sample.json") # 西国分寺→渋谷の乗換有のレスポンス
+    # @route_result = SearchNavitimeRoutesService.sample_fetch(file_name)
     
     # 経路検索結果に引き渡すBookmarkモデルのインスタンス変数
     if user_signed_in?
