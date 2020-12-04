b671a7c1026bc03dae7768e6086b91111fdba24c
nnoitra05 <nexcoxin@gmail.com>
2020-12-04 12:06:45 +0900

    ユーザー削除に伴うブックマーク及びカレンダーの削除

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,7 +1,8 @@
 class Calendar < ApplicationRecord
+
   belongs_to :user
-  has_many :bookmark_calendars
-  has_many :bookmarks, through: :bookmark_calendars, dependent: :destroy
+  has_many :bookmark_calendars, dependent: :destroy
+  has_many :bookmarks, through: :bookmark_calendars
 
   validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookmarks
-  has_many :calendars
+         
+  has_many :bookmarks, dependent: :destroy
+  has_many :calendars, dependent: :destroy
 end
