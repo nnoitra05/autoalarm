e74bdbe947d24b00b86c626108572fc670855cfe
jxn1156596 <jxn1156596@hotmail.com>
2020-12-08 11:36:32 +0900

    window.onloadの追加

--- app/javascript/modal.js
+++ app/javascript/modal.js
@@ -22,44 +22,44 @@
 
 ********************************************************************************/
 
+window.onload = function () {
+  $(function(){
 
-$(function(){
-
-  //モーダルウィンドウを出現させるクリックイベント
-  $("#modal-open").click( function(){
+    //モーダルウィンドウを出現させるクリックイベント
+    $("#modal-open").click( function(){
   
-    //キーボード操作などにより、オーバーレイが多重起動するのを防止する
-    $( this ).blur() ;	//ボタンからフォーカスを外す
-    if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
-    //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
+      //キーボード操作などにより、オーバーレイが多重起動するのを防止する
+      $( this ).blur() ;	//ボタンからフォーカスを外す
+      if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
+      //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
   
-    //オーバーレイを出現させる
-    $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
-    $( "#modal-overlay" ).fadeIn( "slow" ) ;
+      //オーバーレイを出現させる
+      $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+      $( "#modal-overlay" ).fadeIn( "slow" ) ;
   
-    //コンテンツをセンタリングする
-    centeringModalSyncer() ;
+      //コンテンツをセンタリングする
+      centeringModalSyncer() ;
   
-    //コンテンツをフェードインする
-    $( "#modal-content" ).fadeIn( "slow" ) ;
+      //コンテンツをフェードインする
+      $( "#modal-content" ).fadeIn( "slow" ) ;
   
-    //[#modal-overlay]、または[#modal-close]をクリックしたら…
-    $( "#modal-overlay,#modal-close" ).unbind().click( function(){
+      //[#modal-overlay]、または[#modal-close]をクリックしたら…
+      $( "#modal-overlay,#modal-close" ).unbind().click( function(){
   
-      //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-      $( "#modal-content,#modal-overlay" ).fadeOut( "slow" , function(){
+        //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+        $( "#modal-content,#modal-overlay" ).fadeOut( "slow" , function(){
   
-        //[#modal-overlay]を削除する
-        $('#modal-overlay').remove() ;
+          //[#modal-overlay]を削除する
+          $('#modal-overlay').remove() ;
+  
+        } ) ;
   
       } ) ;
   
     } ) ;
   
-  } ) ;
-  
-  //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-  $( window ).resize( centeringModalSyncer ) ;
+    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
+    $( window ).resize( centeringModalSyncer ) ;
   
     //センタリングを実行する関数
     function centeringModalSyncer() {
@@ -70,8 +70,8 @@ $(function(){
   
       // コンテンツ(#modal-content)の幅、高さを取得
       // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-  //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-  //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
+      //var cw = $( "#modal-content" ).outerWidth( {margin:true} );
+      //var ch = $( "#modal-content" ).outerHeight( {margin:true} );
       var cw = $( "#modal-content" ).outerWidth();
       var ch = $( "#modal-content" ).outerHeight();
   
@@ -81,4 +81,4 @@ $(function(){
     }
   
   } ) ;
-  
+};
