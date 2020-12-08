60e23b76a2ade9d378190fd0e80e6ff7d90b9367
jxn1156596 <jxn1156596@hotmail.com>
2020-12-08 22:44:10 +0900

    直通運転のアラームも完成した

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -25,9 +25,9 @@ class TrainsController < ApplicationController
 
     # NAVITIME APIのコール回数削減のために経路検索結果をdumpしたものです。特に不要な場合はこちらのメソッドで読込してください。
     # 直通か非直通かでそれぞれjsonファイルが違うので、目的に合わせてコメントアウトを外してください。
-    file_name = Rails.public_path.join("jsons", "response_sample_no_transit.json") # 所沢→渋谷の直通経路のレスポンス
-    # file_name = Rails.public_path.join("jsons", "response_sample.json") # 西国分寺→渋谷の乗換有のレスポンス
-    @route_result = SearchNavitimeRoutesService.sample_fetch(file_name)
+    # file_name = Rails.public_path.join("jsons", "response_sample_no_transit.json") # 所沢→渋谷の直通経路のレスポンス
+     file_name = Rails.public_path.join("jsons", "response_sample.json") # 西国分寺→渋谷の乗換有のレスポンス
+     @route_result = SearchNavitimeRoutesService.sample_fetch(file_name)
       
       @failure_comment = "全ての項目を入力してください。"
       @information_list = GetTrainInformationService.fetch
@@ -36,7 +36,7 @@ class TrainsController < ApplicationController
     else
 
       # サービスクラスSearchNavitimeRoutesServiceから条件を満たす最適なルートを取得
-      # @route_result = SearchNavitimeRoutesService.fetch(departure, destination, @datetime, departure_flag)
+      #  @route_result = SearchNavitimeRoutesService.fetch(departure, destination, @datetime, departure_flag)
 
       # NAVITIME APIのコール回数削減のために経路検索結果をdumpしたものです。特に不要な場合はこちらのメソッドで読込してください。
       # 直通か非直通かでそれぞれjsonファイルが違うので、目的に合わせてコメントアウトを外してください。
