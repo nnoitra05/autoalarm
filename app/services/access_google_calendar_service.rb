a34a366447ddb9d05c426dd73852898f2e5d9826
nnoitra05 <nexcoxin@gmail.com>
2020-12-10 17:11:47 +0900

    GoogleCalendarAPIのauthorize関連機能記述（未了）

--- app/services/access_google_calendar_service.rb
+++ app/services/access_google_calendar_service.rb
@@ -2,57 +2,32 @@
 
 class AccessGoogleCalendarService
 
-  OOB_URI = "urn:ietf:wg:oauth:2.0:oob".freeze
-  APPLICATION_NAME = "Google Calendar API Ruby Quickstart".freeze
-  TOKEN_PATH = "token.yaml".freeze
-  SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
-
   def initialize(user)
 
+    # Userモデルにcredentials_jsonの中身を格納するカラムを作成してそこから引っ張ってくるように変更する必要あり
     @CREDENTIALS_JSON = JSON.parse(ENV["CREDENTIALS_JSON"])
 
-    # authorize
-    client_id = Google::Auth::ClientId.from_hash @CREDENTIALS_JSON
-
-    binding.pry
-
-    token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
-    authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
-    user_id = "default"
-    credentials = authorizer.get_credentials user_id
-    if credentials.nil?
-      url = authorizer.get_authorization_url base_url: OOB_URI
-      puts "Open the following URL in the browser and enter the " \
-          "resulting code after authorization:\n" + url
-      code = gets
-      credentials = authorizer.get_and_store_credentials_from_code(
-        user_id: user_id, code: code, base_url: OOB_URI
-      )
-    end
-    credentials
-
-    # Initialize the API
-    service = Google::Apis::CalendarV3::CalendarService.new
-    service.client_options.application_name = APPLICATION_NAME
-    service.authorization = authorize
-
-    # Fetch the next 10 events for the user
-    calendar_id = "primary"
-
-    response = service.list_events(calendar_id,
-                                  max_results:   10,
-                                  single_events: true,
-                                  order_by:      "startTime",
-                                  time_min:      DateTime.now.rfc3339)
-    puts "Upcoming events:"
-    puts "No upcoming events found" if response.items.empty?
-    response.items.each do |event|
-      start = event.start.date || event.start.date_time
-      puts "- #{event.summary} : #{event.location} (#{start})"
-      puts "  - #{event.id}"
-    end
+    @calendar_id = "primary"
+    @service = Google::Apis::CalendarV3::CalendarService.new
+    @service.client_options.application_name = "Google Calendar API Ruby Quickstart"
+    @service.authorization = authorize
 
   end
+
+
+  def authorize
+
+    # 認証用の情報を格納する
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: @CREDENTIALS_JSON
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR)
+    # アクセストークンを持ってくる（失敗したらエラー？）
+    authorizer.fetch_access_token!
+    # authorizerをリターン
+    return authorizer
+
+  end
+
 
   def create(summary, start_time)
 
@@ -69,6 +44,7 @@ class AccessGoogleCalendarService
     }
 
   end
+
 
   def destroy
 
